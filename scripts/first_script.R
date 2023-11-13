21aac85f9310ad88a819aec7e89ed621768da8bd
Kimberley van der Waard <k.i.m.van.der.waard@student.rug.nl>
2023-11-13 12:21:52 +0100

    made a figure

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -2,9 +2,16 @@
 library(renv)
 #restore the versions of the packages used to develop this project
 renv::restore()
+# remember to use pull in Git tab to get changes from github
 
+#load libraries
+library(tidyverse)
 x<- c(1,2,3,4,5)
 y<- c(1,4,3,6,9)
 data <- data.frame(x,y)
-data
-
+# now we use |> instead of %>%
+fig01 <- data |> ggplot(aes(x=x, y=y)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+#save in project/map figures/called fig01 as png, resolution as max for screen
+ggsave("./figures/fig01.png", plot = fig01, width = 1920, height = 1068, units = "px")
